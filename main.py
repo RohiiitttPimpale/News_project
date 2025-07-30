5273154c94b1c30fa4e0e37dae7cf9332cadb771
RohiiitttPimpale <rohitpimpale2095@gmail.com>
2025-07-30 21:40:10 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from pprint import pprint
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap5
+
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app=app)
+
+
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    search_key = request.form.get("enter")
+    if search_key:
+        return redirect(url_for("search",search_value=search_key))
+    parameter = {
+        "apiKey": "ceabec2ec6d44951ac7f21c806f1102b",
+        "language": "en",
+    }
+    response = requests.get("https://newsapi.org/v2/top-headlines", params=parameter)
+    lis = response.json()["articles"]
+    return render_template("home.html",articles=lis, title="Todays Top News")
+
+@app.route("/<search_value>",methods=["GET","POST"])
+def search(search_value):
+    search_key = request.form.get("enter")
+    if search_key:
+       return redirect(url_for("search", search_value=search_key))
+    parameter = {
+        "apiKey": "ceabec2ec6d44951ac7f21c806f1102b",
+        "q": search_value
+    }
+    response = requests.get("https://newsapi.org/v2/everything", params=parameter)
+    lis = response.json()["articles"]
+    return render_template("home.html", articles=lis, title=search_value)
+
+if __name__ == "__main__":
+    app.run(debug=True)
